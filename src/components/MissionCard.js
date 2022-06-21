2702b2904dd0b417331ca1591665ead3684142e9
Carla Uyemura <carla.uyemura@gmail.com>
2022-06-21 17:23:31 -0300

    Requisito 10

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -7,7 +7,7 @@ class MissionCard extends React.Component {
 
     return (
       <div data-testid="mission-card">
-        <h2 data-testid="mission-name">{name}</h2>
+        <p data-testid="mission-name">{name}</p>
         <p data-testid="mission-year">{year}</p>
         <p data-testid="mission-country">{country}</p>
         <p data-testid="mission-destination">{destination}</p>
@@ -18,7 +18,7 @@ class MissionCard extends React.Component {
 
 MissionCard.propTypes = {
   name: PropTypes.string.isRequired,
-  year: PropTypes.number.isRequired,
+  year: PropTypes.string.isRequired,
   country: PropTypes.string.isRequired,
   destination: PropTypes.string.isRequired,
 };
